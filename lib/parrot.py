d8d4dcbee8d8507e68d035d6abff6838cb9022e5
joseMirungu <josephirungu890@gmail.com>
2024-10-29 15:49:07 +0300

    complete

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,4 @@
-def parrot():
-    pass
+def parrot(message="Squawk!"):
+    """Prints and returns the message."""
+    print(message)  # Print the message to the console
+    return message   # Return the message
